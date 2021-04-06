56267bdce1cfe3953b28fe9a75459314ec95850b
shiinamiyuki <xiaochun.tong@mail.utoronto.ca>
2021-04-06 03:08:22 -0400

    add mouse control

--- src/bin/arukas.rs
+++ src/bin/arukas.rs
@@ -1,4 +1,4 @@
-use arukas::{geometry::load_model, glm, render::{Camera, ColorAttachment, LookAtCamera, Mesh, OribitalCamera, Perspective, RenderContext, RenderInput, RenderPass, SimpleRenderPass, Size}};
+use arukas::{geometry::load_model, glm, render::{Camera, ColorAttachment, LookAtCamera, Mesh, OribitalCamera, Perspective, RenderContext, RenderInput, RenderPass, SimpleRenderPass, Size, fovx_to_fovy}};
 
 use arukas::render::GPUMesh;
 use winit::{
@@ -19,8 +19,10 @@ struct App {
     ctx: RenderContext,
     size: winit::dpi::PhysicalSize<u32>,
     gpu_meshes: Vec<GPUMesh>,
-    camera: LookAtCamera,
+    camera: OribitalCamera,
     render_pass: SimpleRenderPass,
+    last_cursor_pos: Option<winit::dpi::PhysicalPosition<f64>>,
+    is_key_down: bool,
 }
 impl App {
     fn new(window: &Window) -> App {
@@ -35,17 +37,32 @@ impl App {
             .into_iter()
             .map(|model| GPUMesh::new(&mut ctx, &Mesh::from_triangle_mesh(&model)))
             .collect();
-        let camera = LookAtCamera {
-            eye: glm::vec3(0.0, 0.6, 3.0),
-            center: glm::vec3(0.0, 0.6, 2.0),
-            up: glm::vec3(0.0, 1.0, 0.0),
-            perspective: Perspective {
-                aspect: 16.0 / 9.0,
-                fovy: glm::pi::<f32>() / 2.0,
+        // let camera = LookAtCamera {
+        //     eye: glm::vec3(0.0, 0.6, 3.0),
+        //     center: glm::vec3(0.0, 0.6, 2.0),
+        //     up: glm::vec3(0.0, 1.0, 0.0),
+        //     perspective: Perspective {
+        //         aspect: 16.0 / 9.0,
+        //         fovy: glm::pi::<f32>() / 2.0,
 
-                znear: 0.1,
-                zfar: 100.0,
-            },
+        //         znear: 0.1,
+        //         zfar: 100.0,
+        //     },
+        // };
+        let fovx = 120.0f32.to_radians();
+        let perspective = Perspective {
+            aspect: 16.0 / 9.0,
+            fovy: fovx_to_fovy(fovx, 16.0 / 9.0),
+
+            znear: 0.1,
+            zfar: 100.0,
+        };
+        let camera = OribitalCamera {
+            perspective,
+            center: glm::vec3(0.0, 0.6, 2.0),
+            radius: 1.0,
+            phi: 0.0,
+            theta: glm::pi::<f32>() * 0.5,
         };
         let render_pass = SimpleRenderPass::new(&ctx);
         App {
@@ -54,15 +71,53 @@ impl App {
             gpu_meshes,
             camera,
             render_pass,
+            last_cursor_pos: None,
+            is_key_down: false,
         }
     }
-    fn input(&self, event: &WindowEvent) -> bool {
+    fn input(&mut self, event: &WindowEvent) -> bool {
         match event {
+            WindowEvent::MouseInput {
+                device_id,
+                state: ElementState::Released,
+                button: MouseButton::Left,
+                modifiers,
+            } => {
+                self.is_key_down = false;
+                true
+            }
+            WindowEvent::MouseInput {
+                device_id,
+                state: ElementState::Pressed,
+                button: MouseButton::Left,
+                modifiers,
+            } => {
+                self.is_key_down = true;
+                true
+            }
             WindowEvent::CursorMoved {
                 device_id: _,
                 position,
                 modifiers: _,
-            } => true,
+            } => {
+                if self.last_cursor_pos.is_none() {
+                    self.last_cursor_pos = Some(*position);
+                }
+                if let Some(last_pos) = self.last_cursor_pos {
+                    let diff = glm::vec2(position.x - last_pos.x, position.y - last_pos.y);
+                    let dphi = diff.x / 2000.0 * glm::pi::<f64>();
+                    let dtheta = diff.y / 1000.0 * glm::pi::<f64>();
+                    self.camera.theta += dtheta as f32;
+                    self.camera.phi += dphi as f32;
+                }
+                if self.is_key_down {
+                    self.last_cursor_pos = Some(*position);
+                } else {
+                    self.last_cursor_pos = None;
+                }
+
+                true
+            }
             WindowEvent::KeyboardInput {
                 device_id: _,
                 input,
@@ -98,7 +153,7 @@ fn main() {
     let window = WindowBuilder::new()
         .with_title(String::from("Arukas Engine"))
         .with_resizable(false)
-        .with_inner_size(winit::dpi::PhysicalSize::<u32>::new(1280,720))
+        .with_inner_size(winit::dpi::PhysicalSize::<u32>::new(1280, 720))
         .build(&event_loop)
         .unwrap();
     // Since main can't be async, we're going to need to block
--- src/render/mod.rs
+++ src/render/mod.rs
@@ -16,3 +16,15 @@ pub use texture::*;
 
 #[derive(Debug, Clone, Copy, Default, Eq, PartialEq)]
 pub struct Size(pub u32, pub u32);
+
+pub fn fovy_to_fovx(fovy: f32, aspect: f32) -> f32 {
+    // fieldOfViewX = 2 * atan(tan(fieldOfViewY * 0.5) * aspect)
+    ((fovy * 0.5).tan() * aspect).atan()
+}
+pub fn fovx_to_fovy(fovx: f32, aspect: f32) -> f32 {
+    // fieldOfViewX = 2 * atan(tan(fieldOfViewY * 0.5) * aspect)
+    // fieldOfViewX*0.5 = atan(tan(fieldOfViewY * 0.5) * aspect)
+    // tan(fieldOfViewX*0.5) = tan(fieldOfViewY * 0.5) * aspect
+    // fieldOfViewY = 2 * atan(tan(fieldOfViewX*0.5)/aspect)
+    ((fovx * 0.5).tan() / aspect).atan()
+}
